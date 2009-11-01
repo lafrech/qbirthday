e4347d62875085f00b190e2838c53301a603ced8
Thomas Spura <tomspur@fedoraproject.org>
2009-11-02 00:53:03 +0100

    Optimize install_po.py - less usage of strings
    Signed-off-by: Thomas Spura <tomspur@fedoraproject.org>

--- po/install_po.py
+++ po/install_po.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
-destdir = sys.argv[1]+"/usr/share/locale"
+destdir = sys.argv[1]+"/usr/share/locale/"
 
 for f in os.listdir("."):
     if f.endswith(".po"):
         start = "".join(f.split(".")[:-1])
-        cmd = "msgfmt "+f+" -o "+destdir+"/"+start+"/LC_MESSAGES/gbirthday.mo"
-        #print "calling:", cmd
-        os.popen('mkdir -p "'+ destdir+'/'+start+'/LC_MESSAGES/"')
+        cmd = "msgfmt "+f+" -o "+destdir+start+"/LC_MESSAGES/gbirthday.mo"
+
+        os.popen('mkdir -p "'+ destdir+start+'/LC_MESSAGES/"')
         os.popen(cmd)
