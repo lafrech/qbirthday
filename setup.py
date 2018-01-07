7ba0116a6b8802e5f8f862c3b4bab21ede9c1869
Jérôme Lafréchoux <jerome@jolimont.fr>
2018-01-07 23:57:42 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""Setup file"""
+
+from setuptools import setup, find_packages
+
+
+with open('README') as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
+
+setup(
+    name="QBirthday",
+    version="0.6.8",
+    description="QBirthday birthday reminder",
+    long_description=LONG_DESCRIPTION,
+    author="Jérôme Lafréchoux",
+    author_email="jerome@jolimont.fr",
+    license="GPLv2",
+    keywords="QBirthday birthday reminder",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={'gui_scripts': ['qbirthday = qbirthday:main']},
+    package_data={
+        'qbirthday': ['ui/*.ui', 'pics/*.png', 'i18n/*.qm'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: X11 Applications :: Qt',
+        ('License :: OSI Approved :: '
+         'GNU General Public License v2 or later (GPLv2+)'),
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'PyQt5>=5.7',
+    ],
+    url="https://github.com/lafrech/qbirthday",
+    project_urls={
+        "Bug Tracker": "https://github.com/lafrech/qbirthday/issues",
+        "Source Code": "https://github.com/lafrech/qbirthday",
+    }
+)
